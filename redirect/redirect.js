6d9595d3708da79fa69655c90e58da58b39a09a0
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-13 12:33:31 -0800

    Add files via upload

---
+++ redirect/redirect.js
@@ -0,0 +1,62 @@
+function preload() {
+  redirect = loadTable("redirects.csv","csv","header");
+  loading = loadImage("loading.gif");
+}
+
+var keyword;
+var url;
+
+function setup() {
+  createCanvas(1023,430);  
+  keyword = redirect.getColumn(0);
+  url = redirect.getColumn(1);
+  
+loc = window.location.href;
+
+var foundredirect = false;
+var tickercount = 0;
+
+print(keyword);
+
+if (loc.includes('https://skparab1.github.io/r/')){
+  red = loc.replace('https://skparab1.github.io/r/','');
+  
+  i = 0;
+  scanner = '';
+  while (i <= keyword.length && foundredirect == false){
+    scanner = keyword[i];
+    if (scanner == red){
+      foundredirect = true;
+      openwindow = url[i];
+      openwindow = 'https://'+openwindow;
+    }
+    
+    i += 1;
+  }
+  
+  if (foundredirect){
+    //location.href = openwindow;
+    window.open(openwindow,"_self");
+  } else {
+    window.open("http:skparab1.github.io/wrongredirect","_self");
+  }
+  
+  
+  background(0);
+  textSize(25);
+  stroke(0);
+  fill(255);
+  strokeWeight(3);
+  
+  text('Redirecting to your requested webpage......',100,75);
+  text('This shouldn\'t take too long',100,110);
+
+  
+} else {
+  //window.open("http:skparab1.github.io/wrongredirect","_self");
+}
+}
+
+x = 0;
+
+//
